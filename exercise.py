d116b895f88ae6a106358a6f4f573860cd2f28bf
Jamshid <mamatovjamshid506@gmail.com>
2021-01-23 18:23:31 +0500

    sorted

--- exercise.py
+++ exercise.py
@@ -19,3 +19,14 @@ while x!=5 or y!=5 :
         gender=data['results'][0]['gender']
         name=data['results'][0]['name']['first']
         city=data['results'][0]['location']['city']
+        
+        person={}
+        if gender=='male' and x!=5:
+            x+=1
+            person[name]=city
+            information['males'].append(person)   
+            
+        elif gender=='female' and y!=5 :
+            y+=1
+            person[name]=city
+            information['females'].append(person)
