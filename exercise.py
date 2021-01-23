a09e19007dcbd7b8472d796994afc08f83d9df93
Jamshid <mamatovjamshid506@gmail.com>
2021-01-23 18:20:16 +0500

    if status_code = 200, go to the main part of the code

--- exercise.py
+++ exercise.py
@@ -12,4 +12,10 @@ information['females']=[]
 while x!=5 or y!=5 :
 
     response=r.get("https://randomuser.me/api/")
-    
+
+    if response.status_code==200:
+        data=response.json()
+
+        gender=data['results'][0]['gender']
+        name=data['results'][0]['name']['first']
+        city=data['results'][0]['location']['city']
