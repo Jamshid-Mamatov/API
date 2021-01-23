c244c7f3c890fafb81e16418ffa62ee7270f8ce3
Jamshid <mamatovjamshid506@gmail.com>
2021-01-23 19:58:56 +0500

    I extracted some data from the user data from the data

--- inf_user.py
+++ inf_user.py
@@ -3,3 +3,22 @@ import requests as requests
 import json 
 
 response=requests.get('https://jsonplaceholder.typicode.com/users')
+
+data=response.json()
+
+main_infor={}
+
+
+for user_data in data:
+
+    main_infor[user_data['id']]=[]
+
+    name=user_data['name']
+    email=user_data['email']
+    city=user_data['address']['city']
+    company_name=user_data['company']['name']
+
+    
+
+
+
